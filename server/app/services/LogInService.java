d4feb6c07018096871852639be2d1a5495841fe5
Yamila Glinsek <yamila.glinsek@gmail.com>
2015-09-14 21:21:46 -0300

    Agrego integración con Mongo

--- server/app/services/LogInService.java
+++ server/app/services/LogInService.java
@@ -1,13 +1,24 @@
 package services;
 
 import model.Hello;
+import model.Usuario;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LogInService {
 
     public Hello helloWorld() {
         return new Hello("Hola mundo!!! :D");
+    }
+
+    public List<Usuario> traerTodosLosUsuarios() {
+        return Usuario.traerTodos();
+    }
+
+    public void agregarUsuario(Usuario usuario) {
+        Usuario.crear(usuario);
     }
 
 }
