58dcd1cc4a4e0c9de88f116f8793d45a873a18d2
agrojas <rojasagustin90@gmail.com>
2015-09-21 09:29:40 -0300

    corriendo en celular con android 4.1

---
+++ app/src/main/java/com/rojasa/agu/redDeMascotas/HelloActivity.java
@@ -0,0 +1,46 @@
+package com.rojasa.agu.redDeMascotas;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HelloActivity.this, MenuPrincipal.class));
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hello, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ app/src/main/java/com/rojasa/agu/redDeMascotas/MenuPrincipal.java
@@ -0,0 +1,74 @@
+package com.rojasa.agu.redDeMascotas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MenuPrincipal extends Activity {
+
+    DrawerLayout drawerLayout;
+    ListView listView;
+    String[] opciones = { "Publicar Mascota en Adopción", "Buscar Mascota para adoptar",
+                            "Perdi una Mascota", "Encontré una Mascota." };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        listView.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1,
+                opciones));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                Toast.makeText(MenuPrincipal.this, "Item: " + opciones[arg2],
+                        Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        // Mostramos el botón en la barra de la aplicación
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /*
+	 * mostramos/ocultamos el menu al precionar el icono de la aplicacion
+	 * ubicado en la barra XXX
+	 */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (drawerLayout.isDrawerOpen(listView)) {
+                    drawerLayout.closeDrawers();
+                } else {
+                    drawerLayout.openDrawer(listView);
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu_principal, menu);
+        return true;
+    }
+
+
+}
